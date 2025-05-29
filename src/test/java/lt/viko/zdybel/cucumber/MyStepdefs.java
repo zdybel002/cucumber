92d0086e7b1cd612f3a92b7f538c4b1a40f1f219
Roman Zdybel <s060429@ad.viko.lt>
2025-05-29 17:44:01 +0300

    little change

--- src/test/java/lt/viko/zdybel/cucumber/MyStepdefs.java
+++ src/test/java/lt/viko/zdybel/cucumber/MyStepdefs.java
@@ -1,13 +1,30 @@
 package lt.viko.zdybel.cucumber;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertEquals;
 
 public class MyStepdefs {
 
-
+    MyClass myClass = new MyClass();
     private String today;
+
+    private String actualAnswer;
+
     @Given("today is Sunday")
     public void todayIsSunday() {
-        today = "Sunday"
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void iAskWhetherItSFridayYet() {
+        actualAnswer = myClass.decideWhetherIsFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void iShouldBeTold(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
     }
 }
